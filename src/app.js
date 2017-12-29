e13b95aafe93bd85136788b364847ee712616343
Aimable <niyoai@yahoo.fr>
2017-12-29 16:32:18 +0000

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+import { getCharacters} from "./reducer/characters/actions";
+
+
+// creating a store.
+// this include chrome dev tool extension.
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension(): f => f
+));
+
+store.dispatch(getCharacters());
+
+require('./index.html');
+
+const container = document.getElementById('app-container');
+ReactDOM.render(
+    <AppContainer>
+        <Provider store={store}>
+           <App />
+        </Provider>
+    </AppContainer>,
+container
+);
+
+if (module.hot){
+    module.hot.accept('./components/App', () => {
+        ReactDOM.render(
+            <AppContainer>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </AppContainer>
+            , container
+        );
+    });
+}
---
+++ src/components/CharacterList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';// connects components to the store.
+import {
+    setCurrentCharacter,
+    getCharacterProfile
+} from "../reducer/character/actions";
+
+
+
+const CharacterList = ({ characters, setCharacter }) =>
+    <div id="character-list" className="col-md-6">
+        <h1>Characters</h1>
+        <ul>
+            {characters.map((c , i) =>
+                <li
+                    key={c.name} onClick={setCharacter(i + 1)}
+                >
+                    {c.name}
+                </li>
+            )}
+        </ul>
+    </div>;
+
+const mapStateToProps = ({ characters}) => ({
+    characters
+});
+
+const mapDispatchToProps = dispatch => ({
+    setCharacter(id) {
+        return () => {
+            dispatch(setCurrentCharacter(id));
+            dispatch(getCharacterProfile(id));
+        };
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps) (CharacterList);
